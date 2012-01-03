7a1b2c37b33596998be453a6dc7407a65f6c0ed4
Jena Birch <jena@jenabirch.co.uk>
2012-01-03 16:54:44 +0000

    Added hyphen to "comment-form" id

--- wp-content/themes/html5-boilerplate/comments.php
+++ wp-content/themes/html5-boilerplate/comments.php
@@ -60,7 +60,7 @@
   <p>You must be <a href="<?php echo wp_login_url( get_permalink() ); ?>">logged in</a> to post a comment.</p>
   <?php else : ?>
 
-  <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+  <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="comment-form">
 
   <?php if ( is_user_logged_in() ) : ?>
 
