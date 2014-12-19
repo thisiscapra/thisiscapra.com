92813608c1dad7fd999485dcb12c4a2b63d1e72b
Ollie Kavanagh <olliek@gmail.com>
2014-12-18 20:54:51 -0500

    initial css setup

--- config.rb
+++ config.rb
@@ -15,7 +15,9 @@
 #
 # With no layout
 # page "/path/to/file.html", :layout => false
-#
+
+page "/sitemap.xml", :layout => false
+
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
 #
@@ -36,16 +38,31 @@
 # activate :automatic_image_sizes
 
 # Reload the browser automatically whenever files change
-# configure :development do
-#   activate :livereload
-# end
+configure :development do
+  activate :livereload, :host => "thisiscapra.dev"
+end
 
 # Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
+helpers do
+  def page_title
+    if current_page.data.title
+      "#{current_page.data.title} - Capra"
+    else
+      "Capra"
+    end
+  end
+end
+
+# Fix bug with build errors and typography fonts
+ignore 'fonts/*'
+
+after_build do |builder|
+  print "After_build fixes... "
+  FileUtils.cp_r(Dir['source/fonts/'],'build/')
+  puts "done."
+end
+
+activate :directory_indexes
 
 set :css_dir, 'stylesheets'
 
@@ -56,16 +73,16 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
-  # activate :asset_hash
+  activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
