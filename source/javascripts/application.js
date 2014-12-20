3c40695c1d815cc22aebf74ab10ec96f32e8413d
Ollie Kavanagh <olliek@gmail.com>
2014-12-20 17:08:29 -0500

    Image hover effects

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -24,9 +24,23 @@ if ( 'querySelector' in document && 'addEventListener' in window ) {
 
   // Adding random classes to the homepage
   if(hasClass(body, 'page-index') ) {
-    var bodyClasses = ['alfred','trickets','eightfifty','alton-convent'];
+    var bodyClasses = ['alfred-app','trickets','sports-digest-8-50','alton-convent'];
     var randomClass = Math.floor(Math.random()*bodyClasses.length);
     addClass(body, bodyClasses[randomClass]);
   } 
+
+  // Fading background colours on the work page
+  var clients = document.getElementsByClassName("client");
+  [].forEach.call(clients, function(client){
+    client.addEventListener("mouseover", clientHover, false);
+    client.addEventListener("mouseout", clientHoverOut, false);
+    var clientID = client.getAttribute("id");
+  });
+  function clientHover(evt) {
+    addClass(body, this.getAttribute("id"));
+  }
+  function clientHoverOut(evt) {
+    removeClass(body, this.getAttribute("id"));
+  }
 
 }
