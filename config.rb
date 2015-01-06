bac3a81cbee0095d3e77072814cc274bd317a509
Ollie Kavanagh <olliek@gmail.com>
2015-01-06 12:25:07 -0500

    Styling updates

--- config.rb
+++ config.rb
@@ -66,6 +66,7 @@ end
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload, :host => "thisiscapra.dev", :apply_js_live => true, :apply_css_live => true
+  config[:file_watcher_ignore] += [ /^build\// ]
 end
 
 # Methods defined in the helpers block are available in templates
